b42440f66e7028bc6770d623ad6d12e6c0d7ef53
AshCorr <ashcorr20@gmail.com>
2025-07-28 16:31:25 +0100

    feat: add dev environment to riffraff

--- packages/cdk/bin/cdk.ts
+++ packages/cdk/bin/cdk.ts
@@ -3,6 +3,11 @@ import { GuRoot } from '@guardian/cdk/lib/constructs/root';
 import { EireneV2 } from '../lib/eirene-v2';
 
 const app = new GuRoot();
+new EireneV2(app, 'EireneV2-euwest-1-DEV', {
+	stack: 'discussion',
+	stage: 'CODE',
+	env: { region: 'eu-west-1' },
+});
 new EireneV2(app, 'EireneV2-euwest-1-CODE', {
 	stack: 'discussion',
 	stage: 'CODE',
